9043ecbe377a5c35767f46e169ad31e11d58e8e4
Aaron Sarazan <aaron@sarazan.net>
2020-01-04 19:23:52 -0800

    suspendlock gets internal

--- src/commonMain/kotlin/net/sarazan/bismarck/util/concurrency/SuspendLock.kt
+++ src/commonMain/kotlin/net/sarazan/bismarck/util/concurrency/SuspendLock.kt
@@ -2,7 +2,7 @@ package net.sarazan.bismarck.util.concurrency
 
 import kotlinx.coroutines.yield
 
-class SuspendLock(isLocked: Boolean = false) {
+internal class SuspendLock(isLocked: Boolean = false) {
 
     var isLocked = isLocked
         private set
