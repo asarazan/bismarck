1a07e703de3129adff4bf8460de612f4ce0235a2
Aaron Sarazan <aaron@sarazan.net>
2020-01-05 09:19:39 -0800

    need to figure out dispatcher

---
+++ ios/BismarckTest/BismarckTest/AppDelegate.swift
@@ -0,0 +1,45 @@
+//
+//  AppDelegate.swift
+//  BismarckTest
+//
+//  Created by Aaron Sarazan on 1/5/20.
+//  Copyright © 2020 Aaron Sarazan. All rights reserved.
+//
+
+import UIKit
+import bismarck
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let bis = BaseBismarck<NSString>()
+        assert(bis.peek() == nil)
+        assert(bis.peekState() == .stale)
+        bis.insert(data: "FOO")
+        assert(bis.peek() == "FOO")
+        
+        
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ ios/BismarckTest/BismarckTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BismarckTest
+//
+//  Created by Aaron Sarazan on 1/5/20.
+//  Copyright © 2020 Aaron Sarazan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
